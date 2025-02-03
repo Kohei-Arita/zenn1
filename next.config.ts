377208d0d8b91d690855fa672d1a9df7deba6382
Tki256 <taiki900824@gmail.com>
2025-02-03 10:54:20 +0900

    configを環境変数読み込むように修正

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY || '',
+  },
 };
 
 export default nextConfig;
